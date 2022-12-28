82372879f9c6d531cc68b92093b94a2e5bfb8e52
codewithemmy <codewithemmy@gmail.com>
2022-12-28 18:23:36 +0000

    second commit

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 require("dotenv").config();
 require("express-async-errors");
+const mongoose = require('mongoose')
+mongoose.set("strictQuery", true);
 
 // extra security packages
 // const helmet = require('helmet');
@@ -10,15 +12,15 @@ const cors = require("cors");
 const express = require("express");
 const app = express();
 
-// const connectDB = require("./db/connect");
+const connectDB = require("./db/connect");
 // const authenticateUser = require("./middleware/authentication");
 
 // routers
-// const authRouter = require("./routes/auth");
+const authRouter = require("./routes/auth");
 
 // error handler
-// const notFoundMiddleware = require("./middleware/not-found");
-// const errorHandlerMiddleware = require("./middleware/error-handler");
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
 
 //serve exprss json
 app.use(express.json());
@@ -28,17 +30,18 @@ app.use(cors());
 app.get("/", (req, res) => {
   res.send("hello");
 });
-// app.use("/api/v1/auth", authRouter);
 
-//use not found and errorHandler middleware
-// app.use(notFoundMiddleware);
-// app.use(errorHandlerMiddleware);
+app.use("/api/v1/auth", authRouter);
+
+//serve exprss json
+app.use(express.json());
+app.use(cors());
 
 const port = process.env.PORT || 5000;
 
 const start = async () => {
   try {
-    // await connectDB(process.env.MONGO_URI);
+    await connectDB(process.env.MONGO_URI);
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
