4fd29db2810cc4244c6b4b21e930f564d968b97d
codewithemmy <codewithemmy@gmail.com>
2022-12-30 14:45:18 +0000

    oauth2 changes

--- utils/passportAuth.js
+++ utils/passportAuth.js
@@ -2,7 +2,7 @@ const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth2").Strategy;
 
 const GOOGLE_CLIENT_ID =
-  "764318071938-otluemiqpa2hoodo2kn0uqrk1kfu3i7d.apps.googleusercontent.com";
+  "764318071938-1ihr52ldfttj2evsih7iiqqmk5fab7bf.apps.googleusercontent.com";
 
 const GOOGLE_CLIENT_SECRET = "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";
 
@@ -20,6 +20,8 @@ passport.use(
   )
 );
 
+//https://dish-demo.onrender.com/auth/google/callback
+//      callbackURL: "http://localhost:5000/auth/google/callback",
 passport.serializeUser(function (user, done) {
   done(null, user);
 });
