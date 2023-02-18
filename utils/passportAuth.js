eb3d6416488f964352ff1605362dffc7ca98d179
codewithemmy <codewithemmy@gmail.com>
2023-02-18 23:08:57 +0000

    recee

--- utils/passportAuth.js
+++ utils/passportAuth.js
@@ -6,8 +6,8 @@ const GoogleStrategy = require("passport-google-oauth2").Strategy;
 passport.use(
   new GoogleStrategy(
     {
-      clientID: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientID: process.env.LOCAL_GOOGLE_CLIENT_ID,
+      clientSecret: process.env.LOCAL_GOOGLE_CLIENT_SECRET,
       callbackURL: "https://dish-demo.onrender.com/auth/google/callback",
       passReqToCallback: true,
     },
