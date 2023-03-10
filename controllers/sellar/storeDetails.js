0e5798f0614ce6eb47e39d5a22c72453d8deb48b
codewithemmy <codewithemmy@gmail.com>
2023-03-10 13:05:28 +0000

    updates

--- controllers/sellar/storeDetails.js
+++ controllers/sellar/storeDetails.js
@@ -29,37 +29,40 @@ const createStoreDetails = async (req, res) => {
     deliveryFee,
     minimumOrder,
     description,
+    address,
+    email,
+    phone,
   } = req.body;
   const sellar = req.user.userId;
   if (!storeName || !cuisineType || !openHours) {
     return res.status(400).json({ msg: `all fields should be filled` });
   }
 
-  let converts = fs.readFileSync(req.files.image.tempFilePath, "base64");
-  const buffer = Buffer.from(converts, "base64");
+  // let converts = fs.readFileSync(req.files.image.tempFilePath, "base64");
+  // const buffer = Buffer.from(converts, "base64");
 
-  const convert_url = async (req) => {
-    const data = await sharp(buffer).webp({ quality: 20 }).toBuffer();
-    //use clodinary as a promise using the uploadStream method
-    return new Promise((resolve, reject) => {
-      const stream = cloudinary.uploader.upload_stream(
-        { folder: "DEV" },
-        (err, url) => {
-          if (err) {
-            reject(err);
-          } else {
-            resolve(url);
-          }
-        }
-      );
-      bufferToStream(data).pipe(stream);
-    });
-  };
+  // const convert_url = async (req) => {
+  //   const data = await sharp(buffer).webp({ quality: 20 }).toBuffer();
+  //   //use clodinary as a promise using the uploadStream method
+  //   return new Promise((resolve, reject) => {
+  //     const stream = cloudinary.uploader.upload_stream(
+  //       { folder: "DEV" },
+  //       (err, url) => {
+  //         if (err) {
+  //           reject(err);
+  //         } else {
+  //           resolve(url);
+  //         }
+  //       }
+  //     );
+  //     bufferToStream(data).pipe(stream);
+  //   });
+  // };
 
-  const uri = await convert_url(req);
-  // console.log(uri.secure_url);
+  // const uri = await convert_url(req);
+  // // console.log(uri.secure_url);
 
-  fs.unlinkSync(req.files.image.tempFilePath);
+  // fs.unlinkSync(req.files.image.tempFilePath);
 
   if (sellar) {
     const storeDetails = await StoreDetails.create({
@@ -69,10 +72,13 @@ const createStoreDetails = async (req, res) => {
       openHours: openHours,
       deliveryFee: deliveryFee,
       rating: 0,
+      address,
+      email,
+      phone,
       minimumOrder: minimumOrder,
       description: description,
       serviceAvailable: false,
-      storeImage: uri.secure_url,
+      // storeImage: uri.secure_url,
       storeOwner: sellar,
     });
 
