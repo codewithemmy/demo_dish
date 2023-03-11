00032d355fc8da57e933a224f483d0f37580fb18
codewithemmy <codewithemmy@gmail.com>
2023-03-11 10:36:30 +0000

    ddd

--- models/sellarModel/StoreDetails.js
+++ models/sellarModel/StoreDetails.js
@@ -4,21 +4,18 @@ const StoreDetailsSchema = new mongoose.Schema(
   {
     storeName: {
       type: String,
-      // required: [true, "provide the store name"],
     },
     storeImage: {
       type: String,
     },
     address: {
       type: String,
-      // required: true,
     },
     email: {
       type: String,
     },
     phone: {
       type: String,
-      // required: true,
     },
     location: {
       type: { type: String },
@@ -26,25 +23,20 @@ const StoreDetailsSchema = new mongoose.Schema(
     },
     cuisineType: {
       type: String,
-      // required: [true, "provide your cuisines"],
     },
     deliveryFee: {
       type: String,
-      // required: [true, "provide your delivery fee"],
     },
     minimumOrder: {
       type: String,
-      // required: [true, "provide your minimum order"],
     },
     description: {
       type: String,
-      // required: [true, "description must not be empty"],
     },
     rating: { type: Number },
     openHours: {
       type: String,
       trim: true,
-      // required: [true, "provide hours store is open"],
     },
     serviceAvalaible: { type: Boolean, default: false },
     storeOwner: {
