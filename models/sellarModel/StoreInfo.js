e6f310ac2662c749bb98f226fcecf6b68d114a3f
codewithemmy <codewithemmy@gmail.com>
2023-05-04 13:40:20 +0100

    changes: latest

--- models/sellarModel/StoreInfo.js
+++ models/sellarModel/StoreInfo.js
@@ -4,19 +4,15 @@ const StoreInfoSchema = new mongoose.Schema(
   {
     phoneNumber: {
       type: String,
-      required: [true, "provide store phone number"],
     },
     foodCuisineOffer: {
       type: String,
-      required: [true, "provide the food cuisine you offer"],
     },
     pickUpInstruction: {
       type: String,
-      required: [true, "provide how couriers should pick order"],
     },
     dayOfOpen: {
       type: String,
-      required: [true, "provide your day of open after setup"],
     },
     sellar: {
       type: mongoose.Types.ObjectId,
